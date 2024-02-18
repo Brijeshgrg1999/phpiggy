00e2965a37503098f9b1997483ae8f5ea08cecc2
Gurung brijes <118506279+Brijeshgrg1999@users.noreply.github.com>
2024-02-18 11:32:02 +0800

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php 
+//frameworks foundation -> application class 
+// Router , Vaildator , database , template engine , container . 
+
+
+
+
+
+$app = include __DIR__.'/../src/App/bootstrap.php' ; 
+
+$app->run() ; 
